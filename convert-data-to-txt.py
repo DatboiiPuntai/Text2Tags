286094fa52094d1a99f859ddb79c0110bbcb4d6e
Pantai Suyasri <pantaisuyasri@gmail.com>
2023-04-12 11:03:05 +0700

    added tag scraper i guess

--- convert-data-to-txt.py
+++ convert-data-to-txt.py
@@ -1,16 +1,26 @@
 import json
 import os
 
-DATASET_FILENAME = 'train_data.json'
-DATASET_PATH = 'small_dataset'
+DATASET_FILENAME = 'test_data_alpaca.json'
+DATASET_PATH = 'test_dataset'
 
 with open(os.path.join(DATASET_PATH,DATASET_FILENAME), encoding='utf-8') as f:
     dataset = json.load(f)
 print(f'Loaded {len(dataset)} records from {DATASET_PATH}')
 
+def generate_prompt(data_point):
+    return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+### Instruction:
+{data_point["instruction"]}
+### Input:
+{data_point["input"]}
+### Response:
+{data_point["output"]}"""
 
-for x in dataset:
-    for k, v in x.items():
-        print(f'{k}: {v}')
-        print()
-    print()
+print(generate_prompt(dataset[1]))
+
+# with open('data.txt', 'w') as f:
+#     for data_point in dataset:
+#         prompt = generate_prompt(data_point)
+#         f.write(prompt + '\n\n\n')
+    
