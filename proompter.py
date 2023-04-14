49aabc4d861c6269c3fa26e9ca4f2599682103a7
Pantai Suyasri <pantaisuyasri@gmail.com>
2023-04-14 17:04:20 +0700

    bruh

--- proompter.py
+++ proompter.py
@@ -1,45 +1,64 @@
 import os
 import json
 import random
-from itertools import chain
+import time
 from concurrent.futures import ThreadPoolExecutor
-from pprint import pprint
-from typing import Union
+from tqdm import tqdm
 import openai
 
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-TAG_JSON_DIR = r"dataset\test.json"
+TAG_JSON_DIR = r"dataset\danbooru2021posts.json"
+OUTFILE = r"data_captioned.json"
 MODEL = "gpt-3.5-turbo"
 TEMPERATURE = 0.6
 MAX_THREADS = 10
+BATCH_SIZE = 50
 
+# list of prompt formats to use for generating captions
 PROMPT_FORMATS = [
     "Reply with a complete description of an artworkusing these keywords. Do not infer anything.\n{inputs}",
     "Using these keywords, reply with a creative and eloquent caption.\n{inputs}",
     "Reply with a short and summarized description of an artwork based on these keywords. Do not go over 75 words.\n{inputs}",
-    "Reply with only one description that describes subject, action-pose-expression, hair-eyes-dress-accessories, and background in order using these keywords\n{inputs}"
+    "Reply with only one description that describes subject, action-pose-expression, hair-eyes-dress-accessories, and background in order using these keywords\n{inputs}",
 ]
 
 with open("tag_lookup.txt", "r") as f:
     TAG_LOOKUP = dict(line.strip().split("%") for line in f)
 
-with open(TAG_JSON_DIR, "r") as f:
-    data = json.load(f)
-
 
 def main():
-    with open(TAG_JSON_DIR, "r") as f:
+    with open(TAG_JSON_DIR, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    # openai.api_key = OPENAI_API_KEY
-    # captioned_data = make_data_multithread(data[:5])
-    # with open("captioned_data.json", "w") as outfile:
-    #     json.dump(captioned_data, outfile, indent=4)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    batches = [data[i : i + BATCH_SIZE] for i in range(0, len(data), BATCH_SIZE)]
+
+    captioned_data = []
+
+    with tqdm(total=len(batches), desc="Processing batches") as pbar:
+        for batch in batches:
+            try:
+                captioned_batch = process_batch(batch)
+                captioned_data.extend(captioned_batch)
+                time.sleep(1)
+                pbar.update(1)
+            except:
+                print(f"Error processing batch. Moving on to next batch.")
+
+    with open(OUTFILE, "w") as outfile:
+        json.dump(captioned_data, outfile, indent=4)
+
+
+def process_batch(batch):
+    threads = min(MAX_THREADS, len(batch))
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        captioned_batch = list(tqdm(executor.map(make_data, batch), total=len(batch)))
+    return captioned_batch
 
 
 def replace_general_tags(tags):
-    return ' '.join([TAG_LOOKUP.get(x, x) for x in tags.split(' ')])
+    # replace tags in tag string with their general categories using lookup dictionary
+    return " ".join([TAG_LOOKUP.get(x, x) for x in tags.split(" ")])
 
 
 def generate_inputs(data_point) -> str:
@@ -50,16 +69,15 @@ def generate_inputs(data_point) -> str:
     tag_string_meta = data_point["tag_string_meta"]
 
     return (
-        "artist: " + tag_string_artist + "\n" +
-        "character: " + tag_string_character + "\n" +
-        "copyright: " + tag_string_copyright + "\n" +
+        "artist: " + tag_string_artist + "\n" + 
+        "character: " + tag_string_character + "\n" + 
+        "copyright: " + tag_string_copyright + "\n" + 
         "general: " + tag_string_general + "\n"
-        + "metadata: " + tag_string_meta + "\n"
+        # + "metadata: " + tag_string_meta + "\n"
     )
 
 
-def generate_prompt(inputs: str, prompt_format: Union[str, int] = "random") -> str:
-
+def generate_prompt(inputs: str, prompt_format="random") -> str:
     if type(prompt_format) != str:
         prompt = PROMPT_FORMATS[prompt_format].format(inputs=inputs)
     else:
@@ -67,42 +85,28 @@ def generate_prompt(inputs: str, prompt_format: Union[str, int] = "random") -> s
     return prompt
 
 
-inputs = generate_inputs(data[2])
-print(generate_prompt(inputs, 3))
-
-
 def generate_caption(prompt: str) -> str:
     messages = [{"role": "user", "content": prompt}]
-    # Use the OpenAI API to generate a response using the messages and other parameters
     response = openai.ChatCompletion.create(
         model=MODEL,
         messages=messages,
         temperature=TEMPERATURE,
     )
-    # Get the caption string from the response and return it
+
     caption = response["choices"][0]["message"]["content"]
     return caption
 
 
-def make_data(data_point, prompt_format: str = "random") -> dict:
+def make_data(data_point, prompt_format="random") -> dict:
     inputs = generate_inputs(data_point)
     prompt = generate_prompt(inputs, prompt_format)
     caption = generate_caption(prompt)
 
-    # Add the inputs, prompt, and caption strings to the data_point dictionary
     data_point["prompt"] = prompt
     data_point["caption"] = caption
 
-    # Return the modified data_point dictionary
     return data_point
 
 
-def make_data_multithread(data):
-    threads = min(MAX_THREADS, len(data))
-    with ThreadPoolExecutor(max_workers=threads) as executor:
-        captioned_data = executor.map(make_data, data)
-    return list(captioned_data)
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
