cfad19bfe2a49d8cafa29733854f6f623f5cba6e
Pantai Suyasri <pantaisuyasri@gmail.com>
2023-04-30 18:53:11 +0700

    added human baseline

--- data_to_txt.py
+++ data_to_txt.py
@@ -3,15 +3,15 @@ import os
 
 
 
-with open(os.path.join('dataset','train_data.json'), encoding='utf-8') as f:
+with open(os.path.join('dataset','test_data.json'), encoding='utf-8') as f:
     dataset = json.load(f)
 
 def generate_prompt(data_point):
     return f"""### Caption: {data_point['caption_string']}
 ### Tags: {data_point['tag_string']}"""
 
-with open('data.txt', 'w') as f:
-    for data_point in dataset:
-        prompt = generate_prompt(data_point)
-        f.write(prompt + '\n\n\n')
+new_data = [{'caption_string':x['caption_string'],'tag_string':''} for x in dataset]
+
+with open(r'dataset/human_eval.json', 'w') as f:
+    f.write(json.dumps(new_data,indent=4))
     
